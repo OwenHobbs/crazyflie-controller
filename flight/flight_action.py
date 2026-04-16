580b749e7c2d69e23532d818f9355cdc7a2612b7
Owen Hobbs <owenhobbs2006@gmail.com>
2026-04-16 15:10:03 -0500

    Add on_initial_execution callback

--- flight/flight_action.py
+++ flight/flight_action.py
@@ -15,6 +15,9 @@ class FlightAction(ABC):
         self._flight_runtime: float | None = None
         self._action_runtime: float | None = None
 
+    def on_initial_execution(self):
+        pass
+
     @abstractmethod
     def determine_goal(self) -> Goal | None:
         pass
@@ -26,18 +29,19 @@ class FlightAction(ABC):
         frame: dict[str, Pose],
         flight_runtime: float,
     ) -> tuple[Goal | None, FlightAction | None]:
-        if self._action_start_time is None:
-            self._action_start_time = time.time()
-            self._action_runtime = 0
-            # TODO: could add callback for initial execution
-        else:
-            self._action_runtime = time.time() - self._action_start_time
-
         # Update parameters that could be useful in self.determine_goal
         # Or these values could be passed to determine_goal
         self._drone_pose = drone_pose
         self._frame = frame
         self._flight_runtime = flight_runtime
+
+        if self._action_start_time is None:
+            self._action_start_time = time.time()
+            self._action_runtime = 0
+            # Run callback for initial execution
+            self.on_initial_execution()
+        else:
+            self._action_runtime = time.time() - self._action_start_time
 
         return self.determine_goal(), self._new_action
 
